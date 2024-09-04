db11acfbf3594b056896388fbedcbfd40263f59b
hungv77 <kogome6a@gmail.com>
2024-09-05 00:23:48 +0700

    #2

--- quiz_game.py
+++ quiz_game.py
@@ -2,16 +2,32 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play a game? ")
 
-if playing != "yes" and playing != "Yes":
+if playing.lower() != "yes":
   print("Ok bye?")
   quit()
 
 print("Okay! Let's play, bitch: ")
+score = 0
 
-answer = input("What does CPU stand for? ")
-
-if answer == "Central Processing Unit" :
+answer = input("Just say 1: ")
+if answer.lower() == "1" :
   print('Correct!')
+  score += 1
 else:
   print("Incorrect!")
   
+answer = input("Just say hi: ")
+if answer.lower() == "hi" :
+  print('Correct!')
+  score += 1
+else:
+  print("Dumb bitch!")
+  
+answer = input("Just say 2: ")
+if answer.lower() == "2" :
+  print('Correct!')
+  score += 1
+else:
+  print("Incorrect!")
+  
+print("You got " + str(score) + " questions correct!")
